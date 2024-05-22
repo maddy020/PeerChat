fcc67876134214793d9749852dffd9cc8a5a8ad6
Madhav Setia <madhavsetia24@gmail.com>
2024-05-22 18:55:46 +0530

    Testing the cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,14 +21,14 @@ dbconnection(url)
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", "https://peer-chat-seven.vercel.app"],
+    origin: "https://peer-chat-seven.vercel.app",
     credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://peer-chat-seven.vercel.app"],
+    origin: "https://peer-chat-seven.vercel.app",
   })
 );
 app.use(express.json());
