e7403b9cb70b0dae4549e6e5b6d8207a45c8651d
Madhav Setia <madhavsetia24@gmail.com>
2024-05-22 21:39:19 +0530

    added withCredentials :true

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,20 +12,16 @@ const db_1 = __importDefault(require("./config/db"));
 const http_1 = require("http");
 const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
-const url = process.env.URI;
 const httpServer = (0, http_1.createServer)(app);
-(0, db_1.default)(url)
-    .then(() => {
-    httpServer.listen(8080, () => console.log("Server is running fine"));
-})
-    .catch((err) => console.log("Error in connection to the database", err));
 const io = new socket_io_1.Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://peer-chat-seven.vercel.app",
         credentials: true,
     },
 });
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "https://peer-chat-seven.vercel.app",
+}));
 app.use(express_1.default.json());
 app.use("/auth", auth_1.default);
 app.use("/user", user_1.default);
@@ -53,3 +49,9 @@ io.on("connection", (socket) => {
         }
     });
 });
+const url = process.env.URI;
+(0, db_1.default)(url)
+    .then(() => {
+    httpServer.listen(8080, () => console.log("Server is running fine"));
+})
+    .catch((err) => console.log("Error in connection to the database", err));
